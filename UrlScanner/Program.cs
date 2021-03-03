8a2ef2e77a145c76de1e7346227780662d5ca242
Abdullah Halit Korucuoğlu <halit_korucuoglu@hotmail.com>
2021-03-03 21:40:09 +0300

    Proje dosyası ekle.

---
+++ UrlScanner/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+
+namespace UrlScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.kampustenevar.com/yazar-detay-halit-korucu-";
+            var id = 11465;
+
+            var loop = true;
+
+            while (loop)
+            {
+                Console.WriteLine("ID: {0} Status: Başarısız", id.ToString());
+
+                id += 1;
+                Onay(url + id.ToString());
+
+            }
+
+
+            void Onay(string uri)
+            {
+
+                try
+                {
+                    HttpWebRequest webRequest = (HttpWebRequest)WebRequest
+                                           .Create(uri);
+                    webRequest.AllowAutoRedirect = false;
+                    HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+
+                    var statuscode = (int)response.StatusCode;
+
+                    if (statuscode != 404)
+                    {
+                        Console.WriteLine("Sonuç: {0}", id.ToString());
+
+                        loop = false;
+                    }
+                    else
+                    {
+                        loop = true;
+                    }
+
+
+                }
+
+                catch
+                {
+                    loop = true;
+                }
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
